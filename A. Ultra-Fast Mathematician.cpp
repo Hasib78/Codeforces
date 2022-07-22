092572866348e16f4f4b2e24c856398efb0a4d18
Shaikh Hasibul Islam <islam.hasib78657@gmail.com>
2022-07-22 20:49:55 +0600

    Update A. Ultra-Fast Mathematician.cpp

--- A. Ultra-Fast Mathematician.cpp
+++ A. Ultra-Fast Mathematician.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 int main()
 {
-//    string a,b;
-//    int c[1000],i;
-//    cin>>a>>b;
-//    for(i=0;i<a.length();i++)
-//    {
-//        if(a[i]!=b[i])
-//        {
-//            c[i]=1;
-//        }
-//        else
-//        {
-//            c[i]=0;
-//        }
-//    }
-//    for(i=0;i<a.length();i++)
-//    {
-//        cout<<c[i];
-//    }
+    string a,b;
+    int c[1000],i;
+    cin>>a>>b;
+    for(i=0;i<a.length();i++)
+    {
+        if(a[i]!=b[i])
+        {
+            c[i]=1;
+        }
+        else
+        {
+            c[i]=0;
+        }
+    }
+    for(i=0;i<a.length();i++)
+    {
+        cout<<c[i];
+    }
 
-
-
-
-      int a,b;
-      cin>>a>>b;
-      cout<<(a^b);
 
 }
