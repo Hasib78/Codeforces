4922f637d82adebb618dc9bda03af54de100d8d3
Shaikh Hasibul Islam <islam.hasib78657@gmail.com>
2022-11-13 16:43:37 +0600

    Add files via upload

---
+++ A. The Ultimate Square.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double n;
+    int t,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        x=round(n/2);
+        cout<<x<<endl;
+    }
+}
