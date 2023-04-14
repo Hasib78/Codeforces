89a3672d04325c208d774f8680f04034396e1314
Shaikh Hasibul Islam <islam.hasib78657@gmail.com>
2023-04-14 17:31:41 +0600

    Add files via upload

---
+++ B. Points on Plane.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t,n,a;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        a=sqrt(n-1);
+
+        cout<<a<<endl;
+
+
+    }
+}
